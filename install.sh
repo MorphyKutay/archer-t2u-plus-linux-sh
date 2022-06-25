4eb04fad185eacb1bffb2cdd1e1035f95d6fccb6
MorphyKutay <kutaynevzat2006@gmail.com>
2022-06-25 13:55:25 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,10 @@
+sudo apt update
+sudo apt install dkms git
+echo "deb http://http.kali.org/kali kali-rolling main contrib non-free" | sudo tee /etc/apt/sources.list
+echo "deb http://http.kali.org/kali kali-last-snapshot main contrib non-free" | sudo tee /etc/apt/sources.list
+sudo apt update
+sudo apt install build-essential libelf-dev linux-headers-$(uname -r)
+git clone https://github.com/aircrack-ng/rtl8812au.git
+cd rtl8812au
+sudo make dkms_install
+iwconfig
